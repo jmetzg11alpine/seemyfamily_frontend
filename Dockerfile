2665cba4f40b12b6b0079db5e6178850155c7d7e
jmetzg11alpine <jmetzg11@gmail.com>
2024-10-05 15:40:29 -0700

    ready to deploy

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,8 @@ RUN npm install
 COPY . .
 
 ENV NODE_ENV development
-ENV REACT_APP_API_URL=http://localhost:8000
+# ENV REACT_APP_API_URL=http://localhost:8000
+ENV REACT_APP_API_URL=https://seemyfamily.net/api/
 
 EXPOSE 3000
 
--- src/components/home/helpers.js
+++ src/components/home/helpers.js
@@ -1,6 +1,8 @@
 export const getData = async () => {
   const url = process.env.REACT_APP_API;
   const response = await fetch(url + '/get_main_data');
+  console.log(url);
+  console.log(response);
   const rsp = await response.json();
   return rsp;
 };
